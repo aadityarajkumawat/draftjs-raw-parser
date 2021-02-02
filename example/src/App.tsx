25b6f1df7be6e938c7b5a206fcc7743be08e4470
Aaditya Raj Kumawat <arkumawat78@gmail.com>
2021-02-03 00:16:34 +0530

    found a bug 🐛

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import { ParsedData } from 'draftjs-raw-parser'
-import 'draftjs-raw-parser/dist/index.css'
-import { dataS } from './data'
+// import { dataS } from './data'
+import { convertToRaw, Editor, EditorState, RichUtils } from 'draft-js'
 
 const App = () => {
-  return <ParsedData draftJSRawData={dataS} />
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
+  )
+
+  const postRawContent = convertToRaw(editorState.getCurrentContent()).blocks
+  console.log(postRawContent)
+
+  const handleKeyCommand = (command: any, editorState: any) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command)
+
+    if (newState) {
+      setEditorState(newState)
+      return 'handled'
+    }
+
+    return 'not-handled'
+  }
+  return (
+    <Fragment>
+      <Editor
+        editorState={editorState}
+        onChange={setEditorState}
+        handleKeyCommand={handleKeyCommand}
+        placeholder='Content'
+      />
+      <ParsedData draftJSRawData={JSON.stringify(postRawContent)} />
+    </Fragment>
+  )
 }
 
 export default App
